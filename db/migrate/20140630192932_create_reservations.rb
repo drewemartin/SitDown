a5448e3235752a70bdd64d903f0a60e4c04361f0
Drew Martin <drew.e.martin@gmail.com>
2014-06-30 15:34:36 -0400

    added user,restaurant,reservation models

---
+++ db/migrate/20140630192932_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+    	t.integer :user_id
+    	t.integer :restaurant_id
+    	t.integer :party_size
+    	t.datetime :reservation_time
+
+      t.timestamps
+    end
+  end
+end
