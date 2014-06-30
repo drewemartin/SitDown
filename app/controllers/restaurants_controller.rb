0f8499bfb92529c78aa87358395e226e1a32ba85
Drew Martin <drew.e.martin@gmail.com>
2014-06-30 19:47:45 -0400

    set up user and restaurant controllers

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,41 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_path(@restaurant) :notice => "The changes have been saved"
+    else
+      render 'edit'
+    end
+
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path :notice => "Thank you for adding a restaurant"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :capacity, :user_id)
+  end
+end
